7637d82d237c1c82d77968bf456f64e71fcad270
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-03-20 13:56:19 -0300

    Form structure - new todoForm file - importing it on menu.jsx - creating firt input field and button

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -5,7 +5,7 @@ export default props => (
         <div className='container'>
             <div className='navbar-header'>
                 <a className='navbar-brand' href='#'>
-                    <i className='fa fa-calendar-check-o'></i> React TODO App
+                    <i className='fa fa-asterisk'></i> React TODO App
                 </a>
             </div>
         

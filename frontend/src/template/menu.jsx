9a6f987402bbfd15b9faaae26110353eaeae07a9
Edilson Borges <edilsonfb@gmail.com>
2017-03-20 07:21:54 -0300

    Template menu
    - creating template menu with my interface structure

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-calendar-check-o'></i> React TODO App
+                </a>
+            </div>
+        
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className='nav navbar-nav'>
+                    <li><a href='#/todos'>Tasks</li>
+                    <li><a href='#/about'>About</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)

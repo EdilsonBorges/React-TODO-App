2a6a58204b8979163835457ae16df4ebebd8753b
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-03-20 13:28:24 -0300

    Page Header Component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='About' small='Us'></PageHeader>
+        <h2>Our History</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium error quasi molestias doloremque asperiores laborum, praesentium eos nesciunt omnis ea unde nisi fugit rerum, ipsa non minima voluptatibus perspiciatis!</p>
+        <h2>Mission</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium error quasi molestias doloremque asperiores laborum, praesentium eos nesciunt omnis ea unde nisi fugit rerum, ipsa non minima voluptatibus perspiciatis!</p>
+        <h2>Press</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium error quasi molestias doloremque asperiores laborum, praesentium eos nesciunt omnis ea unde nisi fugit rerum, ipsa non minima voluptatibus perspiciatis!</p>
     </div>
 )

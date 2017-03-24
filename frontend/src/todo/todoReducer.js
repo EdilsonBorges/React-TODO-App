20b536830c8df6d24a5e76c95e9853bd7479b5bd
Edilson Borges <edilsonfb@gmail.com>
2017-03-24 07:40:48 -0300

    Applying Middleware in search load - installing redux-promise - importing promise and applyMiddleware - removing static data from todoReducer - updating description with npm command

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-        description: 'Read book',
-        list: [{
-            _id: 1,
-            description: 'Pay bills',
-            done: true
-        }, {
-            _id: 2,
-            description: 'Meeting',
-            done: false
-        }, {
-            _id: 3,
-            description: 'Medic',
-            done: false
-        }]
-    }
+const INITIAL_STATE = { description: '', list: [] }
 
     export default (state = INITIAL_STATE, action) => {
         switch(action.type) {

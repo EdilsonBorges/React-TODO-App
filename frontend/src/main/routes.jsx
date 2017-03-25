fbed3c27aa180f48c0aaf8b196da0416322bcedd
Edilson Borges <edilson@edilsonborges.com.br>
2017-03-25 18:44:05 -0300

    New page on React TODO App: contact - creating contact jsx - changing routes file

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,11 +3,13 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Contact from '../contact/contact'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/todos' component={Todo} />
         <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
         <Redirect from='*' to='/todos' />
     </Router>
 )

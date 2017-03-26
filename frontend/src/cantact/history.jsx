c0bc7d54f1724b103fa09e73b719a6e4e5fb0542
Edilson Borges <edilsonfb@gmail.com>
2017-03-26 20:36:52 -0300

    New history page

---
+++ frontend/src/cantact/history.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='History' small='Us'></PageHeader>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium error quasi molestias doloremque asperiores laborum, praesentium eos nesciunt omnis ea unde nisi fugit rerum, ipsa non minima voluptatibus perspiciatis!</p>
+    </div>
+)

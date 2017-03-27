d2576f93a6a988e640522f8e2af155e8c3eca031
Edilson Borges <edilsonfb@gmail.com>
2017-03-27 07:03:24 -0300

    Working on action Add - using ES6 destructor - updating button calls - app now can save, but only show when refreshing

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search } from './todoActions'
+import { add,changeDescription, search } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -17,14 +17,21 @@ class TodoForm extends Component {
     }
 
     KeyHandler(e) {
+        const { add, search, description } = this.props
+        /*
+        add = actionCreator
+        search = actionCreator
+        description = data
+        */
         if(e.key === 'Enter') {
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
             props.handleClear()
         }
     }
 
     render() {
+        const { add, search, description } = this.props
         return (
             <div role='form' className='todoForm'>
                 <Grid cols='12 9 10'>
@@ -36,9 +43,9 @@ class TodoForm extends Component {
                 </Grid>
                 <Grid cols='12 3 2'>
                     <IconButton style='primary' icon='plus'
-                        onClick={this.props.handleAdd}></IconButton>
+                        onClick={() => add(description)}></IconButton>
                     <IconButton style='info' icon='search'
-                        onClick={this.props.handleSearch}></IconButton>
+                        onClick={() => serach()}></IconButton>
                     <IconButton style='default' icon='close'
                         onClick={this.props.handleClear}></IconButton>
                 </Grid>
@@ -49,5 +56,5 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ changeDescription, search}, dispatch)
+    bindActionCreators({ add, changeDescription, search}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
